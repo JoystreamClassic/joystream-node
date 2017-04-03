3e34d3daf469aca475f23a7b5b3cd68b8596c390
Lola Rigaut-Luczak <rllola80@gmail.com>
2017-04-03 15:49:47 +0200

    Conanfile LibtorrentNode

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class JoyStreamNodeNPMConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "Libtorrent/1.1.1@joystream/stable", "Extension/0.1@joystream/stable"
+    requires = "LibtorrentNode/1.1.1@joystream/stable", "Extension/0.1@joystream/stable"
     generators = "cmake"
 
     options = {
