eae37bbeb9292824f45c7f7f60f66e160343a20d
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-05-22 17:12:10 +0300

    export enumerations and commitmentToOutput helper function

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ var Session = require('./dist/Session')
 var StateT = require('./dist/StateT')
 var Joystream = require('bindings')('JoyStreamAddon').joystream
 var Libtorrent = require('bindings')('JoyStreamAddon').libtorrent
+var commitmentToOutput = Joystream.commitmentToOutput
+var SESSION_STATE = Joystream.SessionState
+var SESSION_MODE = Joystream.SessionMode
+var BUYING_STATE = Joystream.BuyingState
+var SELLING_STATE = Joystream.SellingState
 
 module.exports = {
   LibtorrentInteraction: Joystream.LibtorrentInteraction,
   InnerStateTypeInfo: Joystream.InnerStateType,
+  SESSION_STATE,
+  SESSION_MODE,
+  BUYING_STATE,
+  SELLING_STATE,
+  TORRENT_STATE: StateT,
   TorrentInfo: Libtorrent.TorrentInfo,
   Session,
-  StateT
+  StateT,
+  PaymentChannel: {
+    commitmentToOutput
+  }
 }
