108b4783d9688c094f41b28f8ad84356139f8de3
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-05-26 19:00:54 +0300

    remove StateT export TorrentState enum

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 var Session = require('./dist/Session')
-var StateT = require('./dist/StateT')
-var Joystream = require('bindings')('JoyStreamAddon').joystream
-var Libtorrent = require('bindings')('JoyStreamAddon').libtorrent
-var commitmentToOutput = Joystream.commitmentToOutput
-var SESSION_STATE = Joystream.SessionState
-var SESSION_MODE = Joystream.SessionMode
-var BUYING_STATE = Joystream.BuyingState
-var SELLING_STATE = Joystream.SellingState
+var joystream = require('bindings')('JoyStreamAddon').joystream
+var libtorrent = require('bindings')('JoyStreamAddon').libtorrent
 
 module.exports = {
-  LibtorrentInteraction: Joystream.LibtorrentInteraction,
-  InnerStateTypeInfo: Joystream.InnerStateType,
-  SESSION_STATE,
-  SESSION_MODE,
-  BUYING_STATE,
-  SELLING_STATE,
-  TORRENT_STATE: StateT,
-  TorrentInfo: Libtorrent.TorrentInfo,
+  // Libtorrent Interaction mode
+  LibtorrentInteraction: joystream.LibtorrentInteraction,
+
+  // Connection State
+  InnerStateTypeInfo: joystream.InnerStateType,
+
+  // Session Mode and State
+  SESSION_MODE: joystream.SessionMode,
+  SESSION_STATE: joystream.SessionState,
+  BUYING_STATE: joystream.BuyingState,
+  SELLING_STATE: joystream.SellingState,
+
+  // Torrent State
+  TORRENT_STATE: libtorrent.TorrentState,
+
+  // Classes
+  TorrentInfo: libtorrent.TorrentInfo,
   Session,
-  StateT,
-  PaymentChannel: {
-    commitmentToOutput
+
+  // Payment channel, helper methods
+  paymentChannel: {
+    commitmentToOutput: joystream.commitmentToOutput
   }
 }
