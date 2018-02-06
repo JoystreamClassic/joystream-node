854f55ce2bc363ccdfaffb81bec12e42dc9e6a4f
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2018-02-06 11:31:13 +0200

    protocol version check to ensure we linked to correct core protocol library

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var Session = require('./dist/Session')
 var joystream = require('bindings')('JoyStreamAddon').joystream
 var libtorrent = require('bindings')('JoyStreamAddon').libtorrent
 
+// Sanity check - ensure we are linking with expected core libraries
+const linked = joystream.protocolVersion
+const expected = 3
+if (linked !== expected) {
+  throw Error('Wrong Protocol Version linked. expected=v' + expected + ', linked=v' + linked)
+}
+
 module.exports = {
   // Libtorrent Interaction mode
   LibtorrentInteraction: joystream.LibtorrentInteraction,
